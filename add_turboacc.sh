c9b84346f5a897a057debbca229aa42af5a3276e
Doty The Cat <dotywrt@gmail.com>
2026-02-12 03:14:03 +0800

    Update add_turboacc.sh

--- add_turboacc.sh
+++ add_turboacc.sh
@@ -60,31 +60,6 @@ rm -rf "$TMPDIR/turboacc/turboacc"
 
 cp -r "$TMPDIR/package/nft-fullcone" "$TMPDIR/turboacc/nft-fullcone" || exit 1
 
-for kernel_version in $kernel_versions; do
-
-    if [ "$kernel_version" = "6.12" ] || \
-       [ "$kernel_version" = "6.6" ] || \
-       [ "$kernel_version" = "6.1" ] || \
-       [ "$kernel_version" = "5.15" ]; then
-        patch_952="952-add-net-conntrack-events-support-multiple-registrant.patch"
-        patch_952_path="./target/linux/generic/hack-$kernel_version/$patch_952"
-    elif [ "$kernel_version" = "5.10" ]; then
-        patch_952="952-net-conntrack-events-support-multiple-registrant.patch"
-        patch_952_path="./target/linux/generic/hack-$kernel_version/$patch_952"
-    else
-        echo "Unsupported kernel version: $kernel_version"
-        exit 1
-    fi
-
-    rm -f "$patch_952_path"
-
-    cp -f "$TMPDIR/package/hack-$kernel_version/$patch_952" "$patch_952_path"
-
-    if ! grep -q "CONFIG_NF_CONNTRACK_CHAIN_EVENTS" "./target/linux/generic/config-$kernel_version"; then
-        echo "# CONFIG_NF_CONNTRACK_CHAIN_EVENTS is not set" >> "./target/linux/generic/config-$kernel_version"
-    fi
-done
-
 cp -r "$TMPDIR/turboacc" "./package/turboacc"
 
 echo "Finish (SFE removed completely)"
